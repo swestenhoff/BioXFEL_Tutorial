81bbbe0571f209b15a936828e83b4d5eaffec7ca
madanmx <madan.mx@gmail.com>
2023-04-05 15:43:35 +0200

    Refine better files

---
+++ refine_extra_LBV.sh
@@ -0,0 +1,188 @@
+#!/bin/bash
+set -e
+
+#unified script for all
+
+###unique name
+ident="1ps_LBV"
+
+###define time points and AF factors to treat. This can produce quite a lot of iterations
+declare -a tps=("1ps")
+
+###set dark model to us
+dark="dark.pdb"
+
+###set param file to use
+param_file="CBD_phenix.params"
+
+
+###wxpert settings
+#set dark refine.mtz to use for (calc dmap scrpt)
+refine=refine.mtz
+echo $refine
+
+declare -a AFs=("25")
+
+#which cif file to use
+#cif="LBV_modified.cif"
+cif="LBV.cif"
+
+
+###create all directories in this one
+basedir="$PWD"
+#basedir=$PWD
+
+###should contain the params file, and the dark files in /dark and extrapolated in extrapol_mtz
+sourcedir="$PWD"
+
+###
+toggle_real_space_refinement="0" # set to 1 real sapce is not tested yet. The idea is from xplor8 - first reciprocal space to get new phases, compute a 2Fext-Fc map and then real space refine. Good idea, but currently we only refine a part of the molecule in rec space, so not sure if this works great. 
+
+#madan
+
+
+
+######### prepare all directories first. This makes sure that all files are present before phenix is run. 
+
+mkdir -p "${basedir}/${ident}/"
+cp "$0" "${basedir}/${ident}/" #copy script into output directory
+cp $param_file "${basedir}/${ident}/" #also parameter file
+
+for tp in "${tps[@]}" 
+do
+
+for AF in "${AFs[@]}"
+   
+do
+
+  tdir=${basedir}/${ident}/${tp}_$AF  
+  echo "in folder" $tdir
+  
+  #make a folder - check if folder is there, if not make one
+  mkdir -p $tdir
+
+  #copy files into folder
+  cp $sourcedir/dark/$dark $tdir/dark.pdb
+  cp $sourcedir/dark/$cif $tdir
+  cp $sourcedir/dark/$refine $tdir/refine.mtz
+
+  cp $sourcedir/extrapolated_mtz/${tp}_extra_${AF}.mtz $tdir
+  cp $param_file $tdir/CBD_phenix.params
+  cp $sourcedir/scripts/mock-dark $tdir
+
+  done
+ #make a run file for coot,  one for each timepoint
+ coottp=run_coot_${tp}.py
+# echo 'set_nomenclature_errors_on_read("auto-correct")' >> $coottp
+# echo 'read_cif_dictionary("${tdir}$cif")' >> $coottp 
+# echo 'handle_read_draw_molecule("${tdir}dark.pdb")” >> $coottp
+
+done
+
+coot="run_coot_all.py"
+#echo 'set_nomenclature_errors_on_read("auto-correct")' >> $coot
+#echo 'read_cif_dictionary("${tdir}$cif")' >> $coot
+#echo 'handle_read_draw_molecule("${tdir}dark.pdb")” >> $coot
+
+#make directory to collect structures and difference maps
+pdir=${basedir}/${ident}/pdbs
+mdir=${basedir}/${ident}/maps
+ldir=${basedir}/${ident}/logs
+mkdir -p $pdir
+mkdir -p $mdir
+mkdir -p $ldir
+
+#prepare the pcc_all.tzt file
+echo "FILE        BproA BproB CproA CproB D207A D207B DA4   DB4   H260A H260B MWA2  PWA3  PWA9  PWB3  PWB9  Rwstar Rfstar Rwfina Rffina " >$pdir/pcc_rec_all.txt
+echo "FILE        BproA BproB CproA CproB D207A D207B DA4   DB4   H260A H260B MWA2  PWA3  PWA9  PWB3  PWB9  Rwstar Rfstar Rwfina Rffina " >$pdir/pcc_real_all.txt
+
+
+#run phenix in target directories. Loop again through the  time points and AFs.
+for tp in "${tps[@]}"
+do
+ for AF in "${AFs[@]}"
+ do
+  tdir=${basedir}/$ident/${tp}_$AF
+  prefix=${ident}_${tp}_${AF}
+  cd $tdir
+  #phenix.refine extra_$tp_12.mtz dark.pdb CBD_phenix.params
+  if [ ! -f "${prefix}_001.pdb" ]; 
+   then
+   echo "running phenix reciprocal space in $tdir"
+   ${seb_phenix}phenix.refine ${tp}_extra_${AF}.mtz monomers.file_name=$cif xray_data.labels="F_${tp},SIGF_${tp}" xray_data.r_free_flags.file_name="${tp}_extra_${AF}.mtz" output.prefix=$prefix CBD_phenix.params --overwrite
+   else
+   echo "not running reciprocal space phenix in $tdir
+ ((force run by deleting $tdir${prefix}_001.pdb))"
+   fi
+
+  if [ $toggle_real_space_refinement == '1' ];
+  then
+    pdb_real="${prefix}_001_real_space_refined_000.pdb" 
+    if [ ! -f $pdb_real ]; 
+     then
+      echo "running phenix real-space in $tdir"
+      ${seb_phenix}phenix.real_space_refine ${prefix}_001.mtz label='2FOFCWT,PH2FOFCWT' ${prefix}_001.pdb $cif CBD_real_space.params --overwrite   
+     else 
+     echo "not running real space phenix in $tdir 
+ ((force run by deleting $tdir${pdb_real}))"
+    fi
+  fi
+
+  #make caclulcated difference map
+  echo "running calc_dmap_b2.sh in $tdir on ${prefix}_001.pdb \n"
+  $sourcedir/scripts/calc_dmap_b2.sh ${prefix}_001.pdb ${tp}_extra_${AF}.mtz ${tp} > $tdir/calc_dmap_b2.log
+
+  echo "running pcc_all.sh in $tdir \n"
+  $sourcedir/scripts/pcc_all.sh ${prefix}_001.pdb_wd.map ${sourcedir}/full_dmaps/${tp}_wd.map
+  
+  rm -rf pcc_out_rec
+  mv pcc_out pcc_out_rec  
+  mv pcc_summary.txt pcc_rec_summary.txt
+
+  #collect output from  pcc_rec_summary.txt
+  printf "%-12s" ${tp}_${AF} >>   $pdir/pcc_rec_all.txt
+  grep -o '[0-9].[0-9][0-9][0-9]' pcc_rec_summary.txt | tr '\n' ' ' >> $pdir/pcc_rec_all.txt
+  
+  #add the Rfree and Rwork from phenix refinement
+  tail -3 ${prefix}_001.log | egrep -o '[0-9]{1}.[0-9]{4}'| tr '\n' ' ' >>   $pdir/pcc_rec_all.txt
+  echo ' ' >>   $pdir/pcc_rec_all.txt
+  ln -f ${prefix}_001.pdb $pdir
+  ln -f ${prefix}_001.pdb_wdex.map $mdir
+  ln -f ${prefix}_001.log $ldir
+
+
+  if [ $toggle_real_space_refinement == '1' ];
+  then
+    echo "running calc_dmap_b2.sh in $tdir on ${pdb_real}"
+    $sourcedir/scripts/calc_dmap_b2.sh $pdb_real ${tp}_extra_${AF}.mtz ${tp} > $tdir/calc_dmap_b2_real_space.log
+    echo "running pcc_all.sh in $tdir"
+    $sourcedir/scripts/pcc_all.sh ${pdb_real}_wd.map ${sourcedir}/full_dmaps/${tp}_wd.map
+
+    rm -rf pcc_out_real
+    mv pcc_out pcc_out_real
+    mv pcc_summary.txt pcc_real_summary.txt
+
+
+    #collect output from  pcc_real_summary.txt
+
+    printf "%-12s" ${tp}_${AF} >>   $pdir/pcc_real_all.txt
+    grep -o '[0-9].[0-9][0-9][0-9]' pcc_real_summary.txt | tr '\n' ' ' >> $pdir/pcc_real_all.txt
+    echo ' ' >>   $pdir/pcc_real_all.txt
+    ln -f ${pdb_real} $pdir
+    ln -f ${pdb_real}_wdex.map $mdir   
+    ln -f ${prefix}_001_real_space_refined_000.log $ldir
+  fi
+
+   done
+done
+
+echo -en "\007"
+echo -en "\007"
+echo -en "\007"
+echo -en "\007"
+
+#fill the coot command file
+#echo "read_map(out_001.pdb)
+
+
+#fill the coot command file 
---
+++ scripts/calc_dmap_b2.sh
@@ -0,0 +1,186 @@
+#!/bin/tcsh -e
+
+# calculate here difference map from calculated FC
+# WITH pases!!!
+# ONLY FCs REQUIRED
+
+#Run the script using the below command
+#./calc_dmap tp_001.pdb tp #where tp is the timepoint 1ps or 10ps 
+#$1 pdb of light $2 mtz light $3 timepoint (for column labels) 
+
+set sdir=$PWD
+
+set dark_model = $sdir/dark.pdb            
+
+set dark_FC = $sdir/refine.mtz               
+
+set light_mtz = $sdir/$2      
+set refi_model = $sdir/$1
+set tim = $3
+#set add = $3
+set resmax = 2.12
+set scalmin = 20.0
+set mapmin = 20.0
+
+set make_nonphased_map = 1 #toggle computation of the nonphased map on and offf
+
+mkdir -p work
+
+#prepare the refine.mtz by changing colum label of sig_dark
+cad             \
+HKLIN1 $dark_FC    \
+HKLOUT work/temp_dark.mtz \
+<< END-cad
+
+LABIN FILE 1 E1=FC_DARK E2=SIGF_DARK E3=PHIC_DARK
+CTYP  FILE 1 E1=F E2=Q E3=P 
+LABOUT FILE 1 E1=F_DARK E2=SIGF_1ps E3=PHIC_1ps
+CTYP  FILE 1 E1=F E2=Q E3=P
+
+END
+END-cad
+
+
+
+sfall HKLIN work/temp_dark.mtz XYZIN $refi_model HKLOUT work/temp_model_tt.mtz << eof-sfall
+
+# Set the mode for structure factor calc. from a xyzin
+      MODE sfcalc hklin xyzin
+      CELL 54.98 116.69 117.86 90.00 90.00 90.00 #
+      LABIN FP=F_DARK SIGFP = SIGF_${tim}
+      LABOUT FC=FC_${tim} PHIC=PHIC_${tim}
+      RESOLUTION 20 $resmax
+      symmetry 19
+      end
+eof-sfall
+
+set light_FC = work/temp_model_tt.mtz
+
+
+# cad things together
+
+
+cad             \
+HKLIN1 $dark_FC    \
+HKLIN2 $light_FC     \
+HKLOUT work/temp_all.mtz \
+<< END-cad
+
+LABIN FILE 1 E1=FC_DARK E2=SIGF_DARK E3=PHIC_DARK
+CTYP  FILE 1 E1=F E2=Q E3=P 
+LABIN FILE 2 E1=FC_${tim} E2=SIGF_${tim} E3=PHIC_${tim}
+CTYP  FILE 2 E1=F E2=Q E3=P
+
+
+END
+END-cad
+
+
+# scale the things
+# labels so far:
+# H K L FC_DARK  PHIC_DARK SIGF_DARK FC_LIGHT SIGF_LIGHT PHIC_LIGHT
+# 1 scale dark to FC dark
+# 2 scale light to dark
+
+
+echo " SCALEIT NUMBER 1, MARIUS CHECK "
+
+
+scaleit \
+HKLIN work/temp_all.mtz    \
+HKLOUT work/temp_all_sc1.mtz    \
+<< END-scaleit1
+TITLE FPHs scaled to FP
+reso $scalmin $resmax      # Usually better to exclude lowest resolution data
+#WEIGHT            Sigmas seem to be reliable, so use for weighting
+#refine anisotropic
+#Exclude FP data if: FP < 5*SIGFP & if FMAX > 1000000
+EXCLUDE FP SIG 4 FMAX 10000000
+#AUTO
+LABIN FP=FC_DARK SIGFP=SIGF_DARK  -
+  FPH1=FC_${tim} SIGFPH1=SIGF_${tim} 
+CONV ABS 0.000001 TOLR  0.00000001 NCYC 100 
+END
+END-scaleit1
+
+freerflag HKLIN work/temp_all_sc1.mtz HKLOUT work/temp_all_sc1_free.mtz <<+
+freerfrac 0.05
++
+
+
+echo "MARIUS non-phased (Fourier approximation) map"
+echo "FIRST FFT ===> "
+
+fft HKLIN work/temp_all_sc1.mtz MAPOUT work/${tim}_diff_nonph.map << endfft
+  RESO $mapmin  $resmax
+  GRID 120 250 250
+  BINMAPOUT
+  LABI F1=FC_${tim} SIG1=SIGF_${tim} F2=FC_DARK SIG2=SIGF_DARK PHI=PHIC_DARK
+endfft
+
+# dump the scaled files to calculate the weighted map
+
+mtz2various HKLIN work/temp_all_sc1.mtz  HKLOUT work/temp_light_scaled.hkl << end_mtzv1
+     LABIN FP=FC_${tim} SIGFP=SIGF_${tim} PHIC = PHIC_${tim}
+     OUTPUT USER '(3I5,3F12.3)'
+     RESOLUTION 33.0 $resmax 
+end_mtzv1
+
+mtz2various HKLIN work/temp_all_sc1.mtz  HKLOUT work/temp_dark_scaled.hkl << end_mtzv2
+     LABIN FP=FC_DARK SIGFP=SIGF_DARK PHIC = PHIC_DARK
+     OUTPUT USER '(3I5,3F12.3)'
+     RESOLUTION 33.0 $resmax 
+end_mtzv2
+
+
+# this is the wmar.inp file FOR DIFF MAPS ONLY FROM FCALC
+
+echo work/temp_light_scaled.hkl > work/temp_wmar.inp
+echo work/temp_light_scaled.hkl >> work/temp_wmar.inp
+echo work/temp_dark_scaled.hkl >> work/temp_wmar.inp
+echo work/temp_${tim}_dark_calc.phs >> work/temp_wmar.inp
+
+
+# run summation program
+# =======================>
+
+echo "Marius get into one file"
+#cp mock-dark.f work/
+#gfortran /home/xfel/tutorials/refine_seb/progs/mock-dark.f -o work/mock-dark 
+./mock-dark < work/temp_wmar.inp
+
+
+#get files back into mtz
+f2mtz HKLIN work/temp_${tim}_dark_calc.phs HKLOUT work/temp_${tim}_dphs.mtz << end_weight 
+CELL  54.980  116.690  117.860  # angles default to 90
+SYMM 19 
+LABOUT H   K  L   FC_D_L DUM  DPHI
+CTYPE  H   H  H   F       Q    P
+END
+end_weight
+
+
+#calculate <phased> difference map
+
+echo
+echo "SECOND FFT ===> "
+
+fft HKLIN work/temp_${tim}_dphs.mtz MAPOUT $1_wd.map << END-wfft
+  RESO 20.0  $resmax 
+  GRID 120 250 250
+  BINMAPOUT
+  LABIN F1=FC_D_L PHI=DPHI
+END-wfft
+
+mapmask mapin $1_wd.map mapout $1_wdex.map xyzin $dark_model << ee
+extend xtal
+border 0.0
+ee
+
+
+
+
+
+
+
+
